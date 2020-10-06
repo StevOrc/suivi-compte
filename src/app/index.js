59c6f5c8a02866f1156d963bf60a94bc3ea1be9c
StevOrc <steven.orcun2@gmail.com>
2020-10-06 13:01:23 +0200

    add UI view and set project architecture

---
+++ src/app/index.js
@@ -0,0 +1,6 @@
+// FROM APP - SHARED
+export { default as Header } from "./components/Header";
+export { default as Balance } from "./components/Balance";
+export { default as IncomeExpenses } from "./components/IncomeExpenses";
+export { default as TransactionList } from "./features/TransactionList";
+export { default as AddTransaction } from "./features/AddTransaction";
